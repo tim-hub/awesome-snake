3b6a9a0113d50cd89e987cd6ffb411b1d59be6e7
tim-hub <tim_w@outlook.com>
2019-03-10 09:38:59 +1300

    update import api

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import os
 import random
 import bottle
 
-from api import ping_response, start_response, move_response, end_response
+from app.api import ping_response, start_response, move_response, end_response
 
 @bottle.route('/')
 def index():
