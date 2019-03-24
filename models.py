1fd666d2e76090c68fb554824de3bddd980be1b3
tim-hub <tim_w@outlook.com>
2019-03-24 21:47:02 +1300

    update model and add migrations

--- models.py
+++ models.py
@@ -10,8 +10,7 @@ class Game(db.Model):
     game_uuid = db.Column(db.String, nullable=False, unique=True)
     game_info = db.Column(JSONB, nullable=False)
     created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    turns = db.relationship('Turn', backref='game', lazy=True)
+    # turns = db.relationship('Turn', backref='game', lazy=True)
     def __repr__(self):
         return '<Game {} - {}>'.format(self.id, self.game_uuid)
 
@@ -19,11 +18,13 @@ class Game(db.Model):
 class Turn(db.Model):
     __tablename__ = 'turn'
     id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
-    turn_uuid = db.Column(db.String, nullable=False, unique=True)
+    turn = db.Column(db.Integer, nullable=False)
     turn_info = db.Column(JSONB, nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    game = db.relationship('Game', backref=db.backref('turns', lazy=True))
+
     def __repr__(self):
-        return '<Turn of Game {},  {} - {}>'.format(self.game_id, self.id, self.turn_uuid)
+        return '<Turn of Game {},  {} - {}>'.format(self.game_id, self.id, self.turn)
 
 
 class GameSchema(ma.Schema):
