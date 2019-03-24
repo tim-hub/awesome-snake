b1ffe5f232b6767f81f98bd691cbd978caaa6116
tim-hub <tim_w@outlook.com>
2019-03-24 19:30:04 +1300

    allow post method for api

--- api.py
+++ api.py
@@ -3,29 +3,29 @@ from flask import request, Response
 from app import app, db, logging
 from models import Game, Turn, GameSchema, TurnSchema
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return '''
     Snake bot is OK, can got to fight now. This is the dev version.
     '''
 
-@app.route('/ping')
+@app.route('/ping', methods=['POST', 'GET'])
 def ping():
     return {'message': 'Server is running.'}
 
-@app.route('/start')
+@app.route('/start', methods=['POST', 'GET'])
 def start():
     return {'message': 'Starting the game.'}
 
 
-@app.route('/move')
+@app.route('/move', methods=['POST', 'GET'])
 def move():
     logging.info(request.data)
     return  { "move": "left" }
 
 
 
-@app.route('/end')
+@app.route('/end', methods=['POST', 'GET'])
 def end():
     return {'message':'Ending the game.'}
 
