cdd885281c45c350903019eae22534ab72cea095
Tim <tim-hub@users.noreply.github.com>
2019-03-30 01:39:53 +0000

    Dev (#2)
    * add a basic map
    
    * remap the map
    
    * refactor to get the map
    
    * refactor bot and ignore the error without db
    
    * change bot color
    
    * refactor codes and make the snake with basic intelligence
    
    * add random to avoid looping
    
    * add description
    
    * add readme

--- api.py
+++ api.py
@@ -1,12 +1,18 @@
-import uuid
+import pprint
 from flask import request, Response
 from app import app, db, logger
 from models import Game, Turn, GameSchema, TurnSchema
+from bot import make_a_decision
+
+pp = pprint.PrettyPrinter(indent=2)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return '''
-    Snake bot is OK, can got to fight now. This is the dev version.
+    Awesome Snake bot is OK, can got to fight now. This is the dev version.
+    
+    
+    This bot is built through Python/Flask, source code at https://github.com/tim-hub/awesome-snake
     '''
 
 @app.route('/ping', methods=['POST', 'GET'])
@@ -23,14 +29,16 @@ def start():
             'you': data.get('you', None),
             'board': data.get('board', None)
         }
-
-        game = Game(game_uuid=game_id, game_info=game_info)
-        db.session.add(game)
-        db.session.commit()
+        try:
+            game = Game(game_uuid=game_id, game_info=game_info)
+            db.session.add(game)
+            db.session.commit()
+        except Exception:
+            logger.debug('db is not running.')
     return {
-        "color": "#ffffff",
-        "headType": "bendr",
-        "tailType": "pixel",
+        "color": "#800000",
+        "headType": "safe",
+        "tailType": "skinny",
         'message': 'Starting the game.'
     }
 
@@ -39,24 +47,45 @@ def start():
 def move():
     logger.info('start a move')
     if request.method =='POST':
+        # get all data
         data = request.data
         game_uuid = data.get("game").get("id", -1)
-        game = Game.query.filter_by(game_uuid=game_uuid).first()
+
         turn_id = data.get("turn", -1)
+        you = data.get('you', None)
+        board = data.get('board', None)
         turn_info = {
-            'you': data.get('you', None),
-            'board': data.get('board', None)
+            'you': you,
+            'board': board
         }
 
-        turn = Turn(turn=turn_id, turn_info=turn_info, game=game)
-        db.session.add(turn)
-        db.session.commit()
-    return  { "move": "left" }
+        try:
+            # store to db
+            game = Game.query.filter_by(game_uuid=game_uuid).first()
+            turn = Turn(turn=turn_id, turn_info=turn_info, game=game)
+            db.session.add(turn)
+            db.session.commit()
+        except Exception:
+            logger.debug('db is not running.')
+        # pp.pprint(data)
+
+    return make_a_decision(board, you)
+
+
+
+
 
 
 
 @app.route('/end', methods=['POST', 'GET'])
 def end():
+    if request.method =='POST':
+        # get all data
+        data = request.data
+        you = data.get('you', None)
+        board = data.get('board', None)
+
+        make_a_decision(board, you)
     return {'message':'Ending the game.'}
 
 
@@ -72,3 +101,4 @@ def admin():
             'scans': TurnSchema(many=True).dump(turns)[0]
         }
     }
+
---
+++ bot.py
@@ -0,0 +1,79 @@
+from functools import reduce
+import random
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
+
+
+def make_a_decision(board, you):
+    # analyze the map and bot
+    height = board.get('height', 0)
+    width = board.get('width', 0)
+    snakes = board.get('snakes', [])
+    all_body_list = [b.get('body', []) for b in snakes]
+    all_bodies = list(reduce(lambda x, y: x + y, all_body_list))
+    food = board.get('food', [])
+
+    the_map, the_map_dict = get_map(height, width, all_bodies, food)
+
+    you_body = you.get('body', [])
+    you_head = you_body[0]
+    print(the_map_dict)
+    return survive(you_head, the_map_dict)
+
+
+def get_map(height, width, all_bodies, food, heads=[]):
+    the_map = []
+    the_map_dict = {}
+    for i in range(height):
+        the_row = []
+        for j in range(width):
+            flag = ' '
+            tmp_pos = {
+                'x': j,
+                'y': i
+            }
+            if tmp_pos in all_bodies:
+                flag = 'b'
+            elif tmp_pos in food:
+                flag = 'f'
+            the_row.append(flag)
+            the_map_dict[(tmp_pos.get('x'), tmp_pos.get('y'))] = flag
+        the_map.append(the_row)
+    pp.pprint(the_map)
+    return the_map, the_map_dict
+
+
+def survive(the_head, the_map_dict):
+    """
+    Survive Strategy, only evaluate the tiles around the head.
+    :param the_head:
+    :param the_map_dict:
+    :return:
+    """
+    x = the_head.get('x')
+    y = the_head.get('y')
+    arounds = [(x, y + 1), (x, y - 1), (x - 1, y), (x + 1, y)]
+
+    directions = {}
+    directions.setdefault((x, y - 1), 'up')
+    directions.setdefault((x, y + 1), 'down')
+    directions.setdefault((x - 1, y), 'left')
+    directions.setdefault((x + 1, y), 'right')
+
+    decisions = {}
+    for around in arounds:
+        decisions.setdefault(around, 0)
+        flag = the_map_dict.get(around, False)
+        if not flag:
+            decisions[around] = -100
+        elif flag == 'f':
+            decisions[around] = 1
+        elif flag == 'b':
+            decisions[around] = -1
+        else:
+            decisions[around] = 0
+    pp.pprint(decisions)
+    decision = max(decisions.keys(), key=(lambda key: decisions[key] + random.random()))
+    print(decision)
+    return {"move": directions[decision]}
